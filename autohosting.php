c615d5c8f722ba6f094b54015c987d2fbfca49fc
Decinco <103062948+Decinco@users.noreply.github.com>
2023-05-15 15:32:08 +0200

    o

---
+++ autohosting.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+
+<form action="post.php" method="post">
+Nombre de usuario: <input type="text" name="name"><br>
+Contraseña: <input type="text" name="pass"><br>
+Confirmar contraseña: <input type="text" name="pass2"><br>
+Subdominio: <input type="text" name="subd"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
