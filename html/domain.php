cceda575f7a9ed7a3152d19306a72dbd668f9879
Decinco <103062948+Decinco@users.noreply.github.com>
2023-05-22 19:31:07 +0200

    stack

--- html/domain.php
+++ html/domain.php
@@ -58,6 +58,8 @@
               else {
                 $query_password = "SELECT * FROM users WHERE username = '" . mysqli_real_escape_string($connection, $_POST["uname"]) . "' AND password = '" . mysqli_real_escape_string($connection, $_POST["pass"]) . "'";
                 $result_password = mysqli_query($connection, $query_psw);
+                echo $query;
+                echo mysqli_num_rows($result);
                 echo $query_password;
                 echo mysqli_num_rows($result_password);
                 if (mysqli_num_rows($result_password) < 1) {
