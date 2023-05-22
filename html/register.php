5e9b12a907970005b3f5972faa817aee6508e35d
sergimediterrania <102745474+sergimediterrania@users.noreply.github.com>
2023-05-22 15:39:51 +0200

    asdas

--- html/register.php
+++ html/register.php
@@ -13,6 +13,7 @@ if (!$connection) {
    <head>
       <meta charset="utf-8">
       <title>Mugueta Brothers</title>
+      <link rel="icon" type="image/x-icon" href="/images/muguetabrothers.ico">
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <link rel="stylesheet" href="../CSS/indice.css" />
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
