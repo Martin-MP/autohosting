9f43fd49bab653850c8d0f47b62cb209522dda13
Decinco <103062948+Decinco@users.noreply.github.com>
2023-05-22 20:02:32 +0200

    turns html post into strings

--- html/domain.php
+++ html/domain.php
@@ -71,6 +71,11 @@
                 $domain_invalid = true;
               }
 
+              if (ctype_alnum(string($_POST["domain"])) = false) {
+                $valid = false;
+                $domain_notalphanumeric = true;
+              }
+
               if (empty($_POST["uname"])) {
                   $valid = false;
               }
