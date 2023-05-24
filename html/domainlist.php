4960ed8efbfe78bfd479c5c1c943eebfea955544
Decinco <103062948+Decinco@users.noreply.github.com>
2023-05-24 19:05:23 +0200

    asdfsdaf

--- html/domainlist.php
+++ html/domainlist.php
@@ -99,7 +99,6 @@
               $domain_result = mysqli_query($connection, $domain_query);
               $selected_user = mysqli_fetch_array($user_result);
               $command = "sudo -n python3 /srv/autohosting/deletehosting.py -u " . $selected_user["user"] . " -d " . $_POST["delete_hosting"] . " 2>&1";
-              echo $selected_user;
               echo $command;
               exec($command, $output, $retval);
               if ($retval == 0) {
