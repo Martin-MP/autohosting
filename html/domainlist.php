0b16d2243143d73f69d638e54a4f39b7cca68311
Decinco <103062948+Decinco@users.noreply.github.com>
2023-05-24 19:09:27 +0200

    adfasfd

--- html/domainlist.php
+++ html/domainlist.php
@@ -92,7 +92,6 @@
             }
 
             if ($_POST["delete_hosting"]) {
-              sleep(7);
               $user_query = "SELECT * FROM domains WHERE domain = '" . mysqli_real_escape_string($connection, $_POST["delete_hosting"]) . "'";
               $user_result = mysqli_query($connection, $user_query);
               $domain_query = "DELETE FROM domains WHERE domain = '" . mysqli_real_escape_string($connection, $_POST["delete_hosting"]) . "'";
@@ -101,6 +100,7 @@
               $command = "sudo -n python3 /srv/autohosting/deletehosting.py -u " . $selected_user["user"] . " -d " . $_POST["delete_hosting"] . " 2>&1";
               echo $command;
               exec($command, $output, $retval);
+              sleep(3);
               if ($retval == 0) {
                 echo "<p>Dominio eliminado correctamente.</p>";
               }
@@ -112,11 +112,11 @@
             }
 
             if ($_POST["delete_user"]) {
-              sleep(7);
               $user_query = "DELETE FROM users WHERE username = '" . mysqli_real_escape_string($connection, $_POST["delete_user"]) . "'";
               $user_result = mysqli_query($connection, $user_query);
               $command = "sudo -n python3 /srv/autohosting/deleteuser.py -u " . $_POST["delete_user"] . " 2>&1";
               exec($command, $output, $retval);
+              sleep(3);
               if ($retval == 0) {
                 $user_deleted = true;
               }
