56d6c9616d0ab5b68599938f91870bac61069dcc
Decinco <103062948+Decinco@users.noreply.github.com>
2023-05-24 18:53:12 +0200

    asdflk

--- html/domainlist.php
+++ html/domainlist.php
@@ -98,7 +98,7 @@
               $domain_query = "DELETE FROM domains WHERE domain = '" . mysqli_real_escape_string($connection, $_POST["delete_hosting"]) . "'";
               $domain_result = mysqli_query($connection, $domain_query);
               $selected_user = mysqli_fetch_assoc($user_result);
-              $command = "sudo -n python3 /srv/autohosting/deletehosting.py -u " . $selected_user . " -d " . $_POST["delete_hosting"] . " 2>&1";
+              $command = "sudo -n python3 /srv/autohosting/deletehosting.py -u " . $selected_user["user"] . " -d " . $_POST["delete_hosting"] . " 2>&1";
               echo $command;
               exec($command, $output, $retval);
               if ($retval == 0) {
